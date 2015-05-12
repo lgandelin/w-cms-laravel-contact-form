1c525f24892a0a9ead6a424929862dd6bd71ea2c
Louis Gandelin <lgandelin@web-access.fr>
2015-05-12 11:47:25 +0200

    Update lang file

--- src/resources/views/partials/blocks/contact-form.blade.php
+++ src/resources/views/partials/blocks/contact-form.blade.php
@@ -1,20 +1,20 @@
 <form action="{{ route('contact_form_controller') }}" method="post">
     <p>
-        <label for="name">Your name</label>
+        <label for="name">{{ trans('w-cms-laravel-contact-form::contact-form.your_name') }}</label>
         <input type="text" id="name" name="name"/>
     </p>
 
     <p>
-        <label for="mail">Your email</label>
+        <label for="mail">{{ trans('w-cms-laravel-contact-form::contact-form.your_email') }}</label>
         <input type="text" id="mail" name="mail"/>
     </p>
 
     <p>
-        <label for="message">Your message</label>
+        <label for="message">{{ trans('w-cms-laravel-contact-form::contact-form.your_message') }}</label>
         <textarea rows="10" id="message" name="message"></textarea>
     </p>
 
-    <input type="submit" value="Send" />
+    <input type="submit" value="{{ trans('w-cms-laravel-contact-form::contact-form.send') }}" />
     <input type="hidden" name="_token" value="{{ csrf_token() }}" />
 </form>
 
